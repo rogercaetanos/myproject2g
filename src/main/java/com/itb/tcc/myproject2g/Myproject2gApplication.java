a550f0e5b36e3152f485d4481d4e4787017604b9
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2023-08-22 09:07:43 -0300

    Ambiente de Desenvolvimento concluído

---
+++ src/main/java/com/itb/tcc/myproject2g/Myproject2gApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.myproject2g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject2gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject2gApplication.class, args);
+	}
+
+}
